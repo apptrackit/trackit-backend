1e96dc76a0c5bdee6df6d514843f7c7c22dafea2
bllbtnd <ballabotond@protonmail.com>
2025-05-22 17:13:53 +0200

    Remove API documentation route

--- routes/docs.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const apiDocs = require('../config/apiDocs');
-
-router.get('/', (req, res) => {
-  res.json(apiDocs);
-});
-
-module.exports = router;
